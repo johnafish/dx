7490ef4a332cd0c926c21e3b76a06f40cbcb8050
johnafish <john@johnafish.ca>
2013-08-28 06:38:16 -0400

    Various Bugfixing Attempts
    Fails, mostly. Progress is being made though. The issue lies in having
    to generate individual random numbers. Will be there very soon though.

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
 var maxValues = new Array();
 var rolledNums = new Array();
 var slot = 0;
-
 function roll(min, max) {
     return Math.round(Math.random() * (max - min) + min);
 }
 function moreDice() {
-    var form = $(document.createElement('form')).appendTo('.jumbotron').attr({role: "form"});
-    var diceLabel = $(document.createElement('input')).appendTo('form').attr({id: "label",type: "text",placeholder: "Enter label", class:"form-control"});
-    var diceMax = $(document.createElement('input')).appendTo('form').attr({id: "maxVal",type: "text",placeholder: "Enter max value", class:"form-control"});
-    var rolled = $(document.createElement('span')).appendTo('form').attr('id', 'rolled');
-    var set = $(document.createElement('input')).appendTo('form').attr({id: 'set', class: 'btn btn-default', type: "button", value: "Set"});
-    var roll = $(document.createElement('input')).appendTo('form').attr({id: 'roll', class: 'btn btn-default', type: "button", value: "Roll"});
-    var pageBreak = $(document.createElement('br')).appendTo('form');
+    var form = $(document.createElement('div')).appendTo('form').attr({class: "form-group "+slot.toString()});
+    var diceLabel = $(document.createElement('input')).appendTo('.'+slot.toString()).attr({id: "label",type: "text",placeholder: "Enter label", class:"form-control"});
+    var diceMax = $(document.createElement('input')).appendTo('.'+slot.toString()).attr({id: "maxVal",type: "text",placeholder: "Enter max value", class:"form-control diceMax "+slot.toString()});
+    var rolled = $(document.createElement('span')).appendTo('.'+slot.toString()).attr('id', 'rolled');
+    var set = $(document.createElement('input')).appendTo('.'+slot.toString()).attr({class: 'btn btn-default set '+slot.toString(), type: "button", value: "Set"});
+    var roll = $(document.createElement('input')).appendTo('.'+slot.toString()).attr({class: 'btn btn-default roll '+slot.toString(), type: "button", value: "Roll"});
+    var pageBreak = $(document.createElement('br')).appendTo('.'+slot.toString());
     slot += 1;
 }
 moreDice();
-$("#maxVal").on("keyup change", function(){
-    maxValues[slot] = this.value;
+$(document).ready(function(){
+    $("input.roll").hide();
+});
+$(".set").click(function(){
+    alert("h");
+    $("input.diceMax."+(slot-1).toString()).replaceWith("<span>d"+maxValues[slot-1]+"=</span>");
+    $("#label").replaceWith("<span>"+label+": </span>");
+    $(this).hide();
+    $(".roll").show();
+});
+$(".diceMax").on("keyup change", function(){
+    maxValues[slot-1] = this.value;
+    alert(maxValues[slot-1]);
 });
 $("#label").on("keyup change", function(){
     label = this.value;
 });
-$("#roll").click(function(){
-    rolledNums[slot]=roll(1, maxValues[slot]);
-    $("#rolled").text(rolledNums[slot]);
+$(".roll").click(function(){
+    rolledNums[slot-1]=roll(1, maxValues[slot-1]);
+    $("#rolled").text(rolledNums[slot-1]);
 });
-$("#set").click(function(){
-    $("#maxVal").replaceWith("<span>d"+maxValues[slot]+"=</span>");
-    $("#label").replaceWith("<span>"+label+": </span>");
-    $(this).hide();
-    $("#roll").show();
+$("#add").click(function(){
+    moreDice();
 });
